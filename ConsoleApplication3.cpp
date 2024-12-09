901982d0d6cb0703e83aaf6f3cc52e960fa1c8f3
akaaws <Askp2@outlook.com>
2024-12-09 09:34:59 +0800

    Add files via upload

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -2,60 +2,9 @@
 #include<iostream>
 #include"types.h"
 using namespace cv;
-//int main()
-//{
-//	Mat ab;
-//	;
-//	
-//	
-//	ab = imread("C:/Users/jkl/Documents/Tencent Files/2662391693/nt_qq/nt_data/Pic/2024-11/Thumb/cfb7948e0939c53001cb4bd16635f6c3_720.jpg");
-//	//图片的截取（rect，circle，explics）
-//	Rect box(100,100, 1100, 800);//（坐标竖，坐标横，宽，长）
-//	Mat ak = ab(box);
-//	imshow("ak", ak);
-//	imshow("ab", ab);
-//
-//	//色彩通道的分离，以灰度图的形式显示，越深其对应的通道值越高
-//	//创立一个新的图篇
-//
-//	Mat ak = Mat::ones(Size(500, 500), CV_8UC3);
-//	ak = Scalar(0, 255, 0);
-//	//cvtColor(ab, aga, COLOR_BGR2HSV);
-//	namedWindow("ak", 0);
-//	std::vector<Mat> somg;
-//	split(ak, somg);
-//	imshow("as", somg[1]);
-//	imshow("as2", somg[2]);
-//	imshow("as3", somg[0]);
-//	//imwrite("C:/Users/jkl/Downloads/try4.jpg",aga);
-//	waitKey(10000);
-//
-//	return 0;
-//}
-
-
-//int main()//摄像头的调用
-//{
-//	VideoCapture cap(0);
-//	while(true)
-//	{
-//		Mat fram;
-//		cap >> fram;
-//		Mat gray;
-//		cvtColor(fram, gray, COLOR_BGR2GRAY);//转灰度图
-//		Mat binary;
-//		threshold(gray, binary, 100, 255, THRESH_BINARY);
-//		imshow("try", binary);
-//		char c = (char)waitKey(9);
-//		if (c == 27)
-//			break;
-//		//imwrite("C:/Users/jkl/Downloads/try12.jpg", fram);
-//	}
-//	return 0;
-//}
 int main()
 {
-	VideoCapture cap("C:/Users/jkl/Downloads/test1.avi");
+	VideoCapture cap("C:/Users/jkl/Downloads/test3.mp4");
 	double chang = cap.get(CAP_PROP_FRAME_HEIGHT);
 	double kuan = cap.get(CAP_PROP_FRAME_WIDTH);
 	double transmit = (chang * kuan) / 25.4;
@@ -116,43 +65,43 @@ int main()
 			lightbars.emplace_back(lightbar);
 	}
 	//尝试通过灯条来识别装甲版，但判断条件单一，识别效果不好
-	std::vector<Armor> armors;
-	for (auto left = lightbars.begin(); left != lightbars.end(); left++)
-	{
-		for (auto right = left + 1; right != lightbars.end(); right++)
-		{
-
-			auto armor = Armor(*left, *right);
-			if (armor.check_Armor())
-				armors.emplace_back(armor);
-		}
-	}
-	//展示框选效果
-	
-	for (const auto& armor : armors)
-	{
-		rectangle(test2, armor.left.top, armor.right.bottom, Scalar(255, 0, 0), 2);
-	}
-	//Mat drawract = yuantu.clone();
-	//for (const auto& roted_rect : roted_rects)
+	//std::vector<Armor> armors;
+	//for (auto left = lightbars.begin(); left != lightbars.end(); left++)
 	//{
-	//	//std::cout << roted_rect.size.height/roted_rect.size.width<<std::endl;//求得长宽比
-	//	Lightbar lightnar=Lightbar(roted_rect);
-	//	
-	//	if (isratioreal(lightnar, 12, 2))
+	//	for (auto right = left + 1; right != lightbars.end(); right++)
 	//	{
-	//		std::vector<Point2f> points(4);
-	//		roted_rect.points(&points[0]);
-	//		//根据所得到的四点画线
-	//		std::vector<std::vector<Point>> contors;
-	//		contors.push_back(std::vector<Point>(points.begin(), points.end()));
-	//		drawContours(drawract, contors, -1, Scalar(255, 0, 0), 2);
+
+	//		auto armor = Armor(*left, *right);
+	//		if (armor.check_Armor())
+	//			armors.emplace_back(armor);
 	//	}
-	//}//查看灯条提取效果
+	//}
+	////展示框选效果
+	//
+	//for (const auto& armor : armors)
+	//{
+	//	rectangle(test2, armor.left.top, armor.right.bottom, Scalar(255, 0, 0), 2);
+	//}
+	Mat drawract = yuantu.clone();
+	for (const auto& roted_rect : roted_rects)
+	{
+		//std::cout << roted_rect.size.height/roted_rect.size.width<<std::endl;//求得长宽比
+		Lightbar lightnar=Lightbar(roted_rect);
+		
+		if (isratioreal(lightnar, 10, 2))
+		{
+			std::vector<Point2f> points(4);
+			roted_rect.points(&points[0]);
+			//根据所得到的四点画线
+			std::vector<std::vector<Point>> contors;
+			contors.push_back(std::vector<Point>(points.begin(), points.end()));
+			drawContours(drawract, contors, -1, Scalar(0, 0, 255), 2);
+		}
+	}//查看灯条提取效果
 	//imshow("try", drawcountours);
 	//imshow("yuantu",gray);
 	
-		imshow("graw", test2);
+		imshow("graw", drawract);
 		char c = (char)waitKey(1);
 		if (c == 27)
 			break;
